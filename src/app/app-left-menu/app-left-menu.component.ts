bb1a70849d8fa7c39b916866b7930fe25582e4eb
Nital Soni <nital.soni@gmail.com>
2020-07-27 13:00:18 +0530

    left menu color fix left menu selection fix - as per angular router

--- src/app/app-left-menu/app-left-menu.component.ts
+++ src/app/app-left-menu/app-left-menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../services/shared.service';
+import { Location } from "@angular/common";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-left-menu',
@@ -7,12 +9,20 @@ import { SharedService } from '../services/shared.service';
   styleUrls: ['./app-left-menu.component.css']
 })
 export class AppLeftMenuComponent implements OnInit {
-  activeTab: string;
+  //activeTab: string;
+  route: string;
 
-  constructor(private sharedService: SharedService) { }
+  constructor(private sharedService: SharedService, private location: Location, private router: Router) {
+    router.events.subscribe(val => {
+      if (val["routerEvent"]) {
+        this.route = val["routerEvent"]["urlAfterRedirects"];
+        //console.log(location.path() + " --> " + this.route);
+      }
+    });
+  }
 
   ngOnInit() {
-    this.activeTab = 'configSearch';
+    //this.activeTab = 'configSearch';
   }
 
   addUserConfig() {
